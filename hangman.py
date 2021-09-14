9fd0d9b6c7435a1cff66e7c2e0f6b82d4851ea4c
Luciano Molon Pfeifer <lppfeifer@gmail.com>
2021-09-14 23:39:18 +0000

    Create function with sets to keep track of letters to be guessed and letters already guessed

--- hangman.py
+++ hangman.py
@@ -1,12 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 from words import words
+import string
+
 
 def get_valid_word(words):
-    word = random.choice(words) #randomly chooses a word from the list
+    word = random.choice(words)  # randomly chooses a word from the list
     while "-" in word or " " in word:
-        word = random.choice(words) #loops trhough the list getting only valid words
+        word = random.choice(words)  # loops trhough list, getting valid words
 
-    return word.upper() #return valid word and uppercase it to make it standard
+    return word.upper()  # return valid word, uppercased to make it standard
+
+
+def hangman():
+    word = get_valid_word(words)  # call get_valid_word function
+    word_letters = set(word)  # set letters in the word
+    alphabet = set(string.ascii_uppercase)  # import pre-dertermined list of uppercased characteres.
+    used_letters = set()  # keep track of what user has guessed
