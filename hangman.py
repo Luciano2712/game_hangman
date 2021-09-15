d28f322f1d1ac8d00d27823838ddb4d1fdafbf9b
Luciano Molon Pfeifer <lppfeifer@gmail.com>
2021-09-15 16:20:30 +0000

    Create game area that is going to be displayed to the User

--- hangman.py
+++ hangman.py
@@ -19,19 +19,24 @@ def hangman():
     alphabet = set(string.ascii_uppercase)  # import pre-dertermined list
     used_letters = set()  # keep track of what user has guessed
 
-    user_letter = input("Guess a letter: ").upper()  # User Input
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)  # Add valid letter
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)  # Remove valid letter from word
+    while len(word_letters) > 0:  # loops until all letters are found
+        # Join and print letters already used
+        print("You have used these letters: ", " ".join(used_letters))
+        # Show what current word is
+        word_l = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_l))
 
-    elif user_letter in used_letters:  # Check for repeated letters
-        print("You have already used this letter. Please try again: ")
+        user_letter = input("Guess a letter: ").upper()  # User Input
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)  # Add valid letter
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)  # Remove letter from word
 
-    else:  # Check for invalid characteres
-        print("Invalid letter. Please try again: ")
+        elif user_letter in used_letters:  # Check for repeated letters
+            print("\nYou have already used this letter. Please try again: ")
+
+        else:  # Check for invalid characteres
+            print("\nInvalid character. Please try again: ")
 
 
-# Getting a letter guess input from the user
-user_input = input("Your guess goes here:")
-print(user_input)
+hangman()
