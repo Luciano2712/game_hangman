4fe4fa3f4cbb4b59c2323c5defebee5436685ce0
Luciano Molon Pfeifer <lppfeifer@gmail.com>
2021-09-15 10:30:18 +0000

    Create if statement to aknowledge input and return error message if input is not valid

--- hangman.py
+++ hangman.py
@@ -20,6 +20,17 @@ def hangman():
     used_letters = set()  # keep track of what user has guessed
 
     user_letter = input("Guess a letter: ").upper()  # User Input
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)  # Add valid letter
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)  # Remove valid letter from word
+
+    elif user_letter in used_letters:  # Check for repeated letters
+        print("You have already used this letter. Please try again: ")
+
+    else:  # Check for invalid characteres
+        print("Invalid letter. Please try again: ")
+
 
 # Getting a letter guess input from the user
 user_input = input("Your guess goes here:")
