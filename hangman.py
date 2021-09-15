afaae042179316a93b955c84d0ef040c7b10fe8c
Luciano Molon Pfeifer <lppfeifer@gmail.com>
2021-09-15 16:45:25 +0000

    Validate code in PEP8 validator (passed)make final adjustments and play game

--- hangman.py
+++ hangman.py
@@ -1,32 +1,33 @@
+# Import tools and libraries
 import random
 from words import words
-import string  # import pre-dertermined list of uppercased characteres
+import string  # Import pre-dertermined list of uppercased characteres
 
 
 # Getting a valid word with only letters from our WORDS list
 def get_valid_word(words):
-    word = random.choice(words)  # randomly chooses a word from the list
+    word = random.choice(words)  # Randomly chooses a word from the list
     while "-" in word or " " in word:
-        word = random.choice(words)  # loops trhough list, getting valid words
+        word = random.choice(words)  # Loops trhough list, getting valid words
 
-    return word.upper()  # return valid word, uppercased to make it standard
+    return word.upper()  # Return valid word, uppercased to make it standard
 
 
 # Keep track of possible guesses and letters already guessed
 def hangman():
-    word = get_valid_word(words)  # call get_valid_word function
-    word_letters = set(word)  # set of letters in the word
-    alphabet = set(string.ascii_uppercase)  # import pre-dertermined list
-    used_letters = set()  # keep track of what user has guessed
+    word = get_valid_word(words)  # Call get_valid_word function
+    word_letters = set(word)  # Set of letters in the word
+    alphabet = set(string.ascii_uppercase)  # Import pre-dertermined list
+    used_letters = set()  # Keep track of what user has guessed
 
-    while len(word_letters) > 0:  # loops until all letters are found
+    while len(word_letters) > 0:  # Loops until all letters are found
         # Join and print letters already used
         print("You have used these letters: ", " ".join(used_letters))
         # Show what current word is
         word_l = [letter if letter in used_letters else "-" for letter in word]
-        print("Current word: ", " ".join(word_l))
-
-        user_letter = input("Guess a letter: ").upper()  # User Input
+        print("\nSecret word: ", " ".join(word_l))
+        # Getting user input
+        user_letter = input("\nGuess a letter: ").upper()  # User Input
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)  # Add valid letter
             if user_letter in word_letters:
@@ -37,6 +38,11 @@ def hangman():
 
         else:  # Check for invalid characteres
             print("\nInvalid character. Please try again: ")
+    # Print when the word is guessed correctly
+    if len(word_letters) == 0:
+        print("\nCongratulations ** YOU WON **")
+        print("The word is", word)
 
 
+# Call function for the game to start
 hangman()
