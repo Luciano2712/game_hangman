fb5516d5e37cb106901d72a30d8d32fb105d2649
Luciano Molon Pfeifer <lppfeifer@gmail.com>
2021-09-15 10:03:50 +0000

    get input from the user and store guessed letters in the variable

--- hangman.py
+++ hangman.py
@@ -1,8 +1,9 @@
 import random
 from words import words
-import string
+import string  # import pre-dertermined list of uppercased characteres
 
 
+# Getting a valid word with only letters from our WORDS list
 def get_valid_word(words):
     word = random.choice(words)  # randomly chooses a word from the list
     while "-" in word or " " in word:
@@ -11,8 +12,15 @@ def get_valid_word(words):
     return word.upper()  # return valid word, uppercased to make it standard
 
 
+# Keep track of possible guesses and letters already guessed
 def hangman():
     word = get_valid_word(words)  # call get_valid_word function
-    word_letters = set(word)  # set letters in the word
-    alphabet = set(string.ascii_uppercase)  # import pre-dertermined list of uppercased characteres.
+    word_letters = set(word)  # set of letters in the word
+    alphabet = set(string.ascii_uppercase)  # import pre-dertermined list
     used_letters = set()  # keep track of what user has guessed
+
+    user_letter = input("Guess a letter: ").upper()  # User Input
+
+# Getting a letter guess input from the user
+user_input = input("Your guess goes here:")
+print(user_input)
